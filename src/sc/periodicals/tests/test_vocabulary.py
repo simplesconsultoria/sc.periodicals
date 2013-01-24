2e895a0cf6f2c54c4410c5e146b46d218e764fb5
Gustavo Lepri <lepri@simplesconsultoria.com.br>
2013-01-24 12:22:34 -0200

    tests for vocabulary included

---
+++ src/sc/periodicals/tests/test_vocabulary.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from sc.periodicals.testing import FUNCTIONAL_TESTING
+from sc.periodicals.vocab import thumbnail_sizes_vocabulary
+
+import unittest2 as unittest
+
+
+class VocabulariesTest(unittest.TestCase):
+
+    layer = FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_thumbnail_sizes_vocabulary(self):
+        self.assertTrue(thumbnail_sizes_vocabulary)
+
+    def test_thumbnail_sizes_vocabulary_quantity(self):
+        vocabulary = thumbnail_sizes_vocabulary(self.portal)
+        self.assertEqual(len(vocabulary.by_token), 7)
+
+    def test_thumbnail_sizes_vocabulary_items(self):
+        vocabulary = thumbnail_sizes_vocabulary(self.portal)
+        clients = ['mini', 'thumb', 'large', 'listing', 'tile', 'preview', 'icon']
+        for term in vocabulary.by_token.keys():
+            self.failIf(term not in clients)
