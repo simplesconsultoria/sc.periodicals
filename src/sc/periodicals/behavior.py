db800efd63a0ea3dd0548706a0144fa7ad6e8299
Gustavo Lepri <lepri@simplesconsultoria.com.br>
2013-01-24 09:52:54 -0200

    view for periodical and behavior fixed

--- src/sc/periodicals/behavior.py
+++ src/sc/periodicals/behavior.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from five import grok
-
 from sc.periodicals.content import IPeriodical
 from sc.periodicals.interfaces import INameFromEdition
-from rwproperty import getproperty, setproperty
-from zope.container.interfaces import INameChooser
 from zope.interface import implements
 from zope.component import adapts
-from Acquisition import aq_inner, aq_base
-
-ATTEMPTS = 100
 
 
 class NameFromEdition(object):
@@ -28,31 +21,3 @@ class NameFromEdition(object):
     @property
     def title(self):
         return str(self.context.number)
-#
-#    def chooseName(self, name, object):
-#        import pdb;pdb.set_trace()
-#        container = aq_inner(self.context)
-#        if not name:
-#            nameFromEdition = INameFromEdition(object, None)
-#            if nameFromEdition is not None:
-#                number = nameFromEdition.number_edition
-#            if not number:
-#                number = getattr(aq_base(object), 'id', None)
-#            if not number:
-#                number = getattr(aq_base(object), 'portal_type', None)
-#            if not number:
-#                number = object.__class__.__name__
-#
-#        return self._findUniqueName(number, object)
-#
-#    def _findUniqueName(self, name, object):
-#        """Find a unique name in the parent folder, based on the given id, by
-#        appending -n, where n is a number greater than 1, or just the id if
-#        it's ok.
-#        """
-#        idx = 1
-#        while idx <= ATTEMPTS:
-#            new_name = "%s-%d%s" % (name, idx)
-#            return new_name
-#
-#        raise ValueError("Cannot find a unique name based on %s after %d attemps." % (name, ATTEMPTS,))
