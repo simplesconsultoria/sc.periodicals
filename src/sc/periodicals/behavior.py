ea8c7dc4947c7667a4bb4aee1ab55689fb76e917
hvelarde <hector.velarde@gmail.com>
2013-02-12 20:43:02 -0200

    rename INameFromEdition behavior to INameFromNumber

--- src/sc/periodicals/behavior.py
+++ src/sc/periodicals/behavior.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from sc.periodicals.content import IPeriodical
-from sc.periodicals.interfaces import INameFromEdition
-from zope.interface import implements
+from sc.periodicals.interfaces import INameFromNumber
 from zope.component import adapts
+from zope.interface import implements
 
 
 class NameFromEdition(object):
     """A name chooser for a Zope object manager.
 
     If the object is adaptable to or provides INameFromEditon, use the
-    edition number to generate a name.
+    periodical number to generate a name.
     """
-    implements(INameFromEdition)
+    implements(INameFromNumber)
     adapts(IPeriodical)
 
     def __init__(self, context):
--- src/sc/periodicals/interfaces.py
+++ src/sc/periodicals/interfaces.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from sc.periodicals import _
-
-from zope.interface import Interface
 from plone.app.content.interfaces import INameFromTitle
-from zope import schema
+from zope.interface import Interface
 
 
 class IPeriodicalLayer(Interface):
@@ -12,15 +9,7 @@ class IPeriodicalLayer(Interface):
     """
 
 
-class INameFromEdition(INameFromTitle):
-    """ A name chooser for a Zope object manager.
+class INameFromNumber(INameFromTitle):
 
-    If the object is adaptable to or provides INameFromEdition, use the
-    edition number to generate a name.
-    """
-
-    number = schema.Int(
-        title=_(u'Edition number'),
-        description=_(u'help_edition_number', default=u'A number for this edition.'),
-        required=True,
-    )
+    def title():
+        """Return a processed title"""
