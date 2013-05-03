de461583d8591127bbc72c69392577e012ee9f66
hvelarde <hector.velarde@gmail.com>
2013-05-03 07:05:58 -0300

    Preparing release 1.0a2

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,7 +1,7 @@
 Changelog
 ---------
 
-1.0a2 (unreleased)
+1.0a2 (2013-05-03)
 ^^^^^^^^^^^^^^^^^^
 
 - Use effective date instead of creation date for getting the latest
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '1.0a2.dev0'
+version = '1.0a2'
 description = "A content type to register periodicals of a printed publication."
 long_description = (
     open("README.rst").read() + "\n" +
