6bb1d0d27c0abc9747d294bb1c2d654c7ee46521
hvelarde <hector.velarde@gmail.com>
2013-09-02 16:21:24 -0300

    Fix package status

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='sc.periodicals',
       description=description,
       long_description=long_description,
       classifiers=[
-          "Development Status :: 1 - Alpha",
+          "Development Status :: 4 - Beta",
           "Environment :: Web Environment",
           "Framework :: Plone",
           "Framework :: Plone :: 4.2",
