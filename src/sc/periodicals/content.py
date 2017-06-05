7211b3f5302b746c928c6b6d835c1db511d903f2
hvelarde <hector.velarde@gmail.com>
2017-06-05 17:14:23 -0300

    Fix quotes on docstrings

--- src/sc/periodicals/content.py
+++ src/sc/periodicals/content.py
@@ -55,7 +55,7 @@ class Periodical(Container):
     grok.implements(IPeriodical)
 
     def image_thumb(self):
-        ''' Return a thumbnail '''
+        """Return a thumbnail."""
         if not has_image(self):
             return None
         view = self.unrestrictedTraverse('@@images')
@@ -63,7 +63,7 @@ class Periodical(Container):
                           scale='thumb').index_html()
 
     def tag(self, scale='thumb', css_class='tileImage', **kw):
-        ''' Return a tag to the image '''
+        """Return a tag to the image."""
         if not has_image(self):
             return ''
         view = self.unrestrictedTraverse('@@images')
