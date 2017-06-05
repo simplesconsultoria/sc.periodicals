a9490fc89cb1f966352256e65a146eb9bff0b9cd
hvelarde <hector.velarde@gmail.com>
2017-06-05 17:46:07 -0300

    Fix test fixture for Plone 5

--- src/sc/periodicals/testing.py
+++ src/sc/periodicals/testing.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
+"""Setup testing infrastructure.
 
+For Plone 5 we need to install plone.app.contenttypes.
+"""
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
+
+import pkg_resources
+
+
+try:
+    pkg_resources.get_distribution('plone.app.contenttypes')
+except pkg_resources.DistributionNotFound:
+    from plone.app.testing import PLONE_FIXTURE
+else:
+    from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE as PLONE_FIXTURE
 
 
 class Fixture(PloneSandboxLayer):
@@ -11,20 +23,17 @@ class Fixture(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
-        # Load ZCML
         import sc.periodicals
         self.loadZCML(package=sc.periodicals)
 
     def setUpPloneSite(self, portal):
-        # Install into Plone site using portal_setup
         self.applyProfile(portal, 'sc.periodicals:default')
 
+
 FIXTURE = Fixture()
+
 INTEGRATION_TESTING = IntegrationTesting(
-    bases=(FIXTURE,),
-    name='sc.periodicals:Integration',
-)
+    bases=(FIXTURE,), name='sc.periodicals:Integration')
+
 FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(FIXTURE,),
-    name='sc.periodicals:Functional',
-)
+    bases=(FIXTURE,), name='sc.periodicals:Functional')
